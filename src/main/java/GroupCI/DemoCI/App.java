eda785019f9c3af47e322f3e450a59c25447cd9b
raehasandalwala <raehasandalwala@gmail.com>
2016-05-08 11:25:44 +0530

    1st commit

---
+++ src/main/java/GroupCI/DemoCI/App.java
@@ -0,0 +1,13 @@
+package GroupCI.DemoCI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
